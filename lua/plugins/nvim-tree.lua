1f9854557bd06a0cc3def49e9ec41f95661a7f8f
Alex Campos <contatooitoo@gmail.com>
2024-11-19 15:05:35 -0300

    Update nvim-tree.lua

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -9,52 +9,6 @@ return {
     vim.g.loaded_netrwPlugin = 1
 
     nvimtree.setup({
-      commands = {
-        system_open = function(state)
-          -- TODO: remove deprecated method check after dropping support for neovim v0.9
-          (vim.ui.open or astro.system_open)(state.tree:get_node():get_id())
-        end,
-        parent_or_close = function(state)
-          local node = state.tree:get_node()
-          if node:has_children() and node:is_expanded() then
-            state.commands.toggle_node(state)
-          else
-            require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
-          end
-        end,
-        child_or_open = function(state)
-          local node = state.tree:get_node()
-          if node:has_children() then
-            if not node:is_expanded() then -- if unexpanded, expand
-              state.commands.toggle_node(state)
-            else -- if expanded and has children, seleect the next child
-              if node.type == "file" then
-                state.commands.open(state)
-              else
-                require("neo-tree.ui.renderer").focus_node(state, node:get_child_ids()[1])
-              end
-            end
-          else -- if has no children
-            state.commands.open(state)
-          end
-        end,
-        copy_selector = function(state)
-          local node = state.tree:get_node()
-          local filepath = node:get_id()
-          local filename = node.name
-          local modify = vim.fn.fnamemodify
-
-          local vals = {
-            ["BASENAME"] = modify(filename, ":r"),
-            ["EXTENSION"] = modify(filename, ":e"),
-            ["FILENAME"] = filename,
-            ["PATH (CWD)"] = modify(filepath, ":."),
-            ["PATH (HOME)"] = modify(filepath, ":~"),
-            ["PATH"] = filepath,
-            ["URI"] = vim.uri_from_fname(filepath),
-          }
-        end,
-      },  
       view = {
         width = 35,
         relativenumber = true,
